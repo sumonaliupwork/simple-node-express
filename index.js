0609575d34852aa10a07d321ea136e82af1d2056
sumonaliupworke <sumonaliupwork@gmail.comm>
2022-07-18 16:13:32 +0600

    ahsan-update

--- index.js
+++ index.js
@@ -33,6 +33,9 @@ app.post('/users', (req, res) => {
     // res.send(JSON.stringify(newUser))
     res.json(newUser);
 })
+app.get('/ahsan/:ahsanId', (req, res) => {
+    res.send('Hi ahsan')
+})
 
 app.get('/users/:id', (req, res) => {
     const id = req.params.id;
